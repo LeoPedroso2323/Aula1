af8058c776f8fe236a77a9e24d7d98963fdff351
LeoPedroso2323 <leonardopedroso857@gmail.com>
2024-08-21 12:40:31 -0300

    21-08-24

--- src/server.js
+++
@@ -1,5 +0,0 @@
-//Importar a instância do Express configurada em index.js
-const app = require("./index");
-//Inicia o servidor na porta 3000, neste caso a Api sera acessivel em http://localhost
-app.listen(3000);
-
