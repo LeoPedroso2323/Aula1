afe18076f33a44046f94954878378d9d6dfc465f
LeoPedroso2323 <leonardopedroso857@gmail.com>
2024-08-21 11:26:51 -0300

    hoje

---
+++ Leonardo21-08/ATIVIDADE/server1.js
@@ -0,0 +1,5 @@
+//Importar a instância do Express configurada em index.js
+const app = require("./index");
+//Inicia o servidor na porta 5000, neste caso a Api sera acessivel em http://localhost
+app.listen(3000);
+
---
+++ Leonardo21-08/src/index.js
@@ -0,0 +1,33 @@
+ //Importa o modulo express
+ const express = require('express')
+
+ //Define uma classe para organizar a lógica da aplicação
+ class AppController{
+
+
+    constructor(){
+        //Cria uma nova instância do Express dentro da classe    
+        this.express = express();
+        
+        //Chama o método middlewares para configurar configurar os middlewares
+        this.middlewares();
+        
+        //Chama o método routes para definir as rotas da Api
+        this.routes();
+    }
+ 
+    middlewares(){
+        //Permitir que a aplicação receba dados em formato JSON nas requisições 
+        this.express.use(express.json());
+ }
+
+    //Defien as rotas da nossa API
+    routes() {
+    //Define uma rota GET para o caminho health
+    this.express.get("/health/", (req,res) => {
+        res.send({ status: "OK"});
+    });//Essa rota é usada para verificar se a Api está OK
+ }
+}
+//Exportando a instância de Express configurada, para que seja
+module.exports = new AppController().express;
